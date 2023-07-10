65c34ce97db3d193f14efd2b4315216165f2cce9
njokujr <njokuvictory216@gmail.com>
2023-07-10 19:11:37 +0000

    task-1 done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""1. Let's execute multiple coroutines at
-the same time with async"""
-
+'''Task 1's module.
+'''
 import asyncio
-import random
 from typing import List
-import itertools as it
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ return the list of all the delays (float values)
-    generated from wait_random"""
-    resp = await asyncio.gather(*(it.repeat(wait_random(max_delay), n)))
-    return list(resp)
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
